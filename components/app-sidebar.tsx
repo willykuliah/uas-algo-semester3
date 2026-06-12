ebba018f70992b27a2c20562448dce37078485b6
wilypradana <pradanawily28@gmail.com>
2026-06-12 11:58:25 +0700

    skip error

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,16 +1,11 @@
 import {
   Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/sidebar";
-import { DropdownMenu } from "radix-ui";
 
 export function AppSidebar() {
   return (
@@ -18,19 +13,17 @@ export function AppSidebar() {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton>
-                  Select Workspace
-                  <ChevronDown className="ml-auto" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
-                <DropdownMenuItem>
-                  <span>Acme Inc</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton>
+                Select Workspace
+                <ChevronDown className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+              <DropdownMenuItem>
+                <span>Acme Inc</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
